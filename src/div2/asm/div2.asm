38c57e58a2e6cee79b45e5e48d263f28c6756e0c
Alexander Wyss <alexsilvan.wyss@gmail.com>
2023-09-21 17:01:15 +0200

    basic single digit division

--- src/div2/asm/div2.asm
+++ src/div2/asm/div2.asm
@@ -7,6 +7,10 @@ extern _exit
 
 ; export entry point
 global _start
+    LENGTH EQU 20
+    section .bss ; uninitialized data
+    alignb 8 ; align to 8 bytes (for 64-bit machine)
+    BUFFER resb LENGTH ; buffer (64 bytes)
 
 section .text
 
@@ -16,6 +20,23 @@ _start:
                                         ; function calls. Required for Windows and MacOS.
 
 ; implement divider (milestone 1)
+            mov rdi, BUFFER ; copy pointer to BUFFER into rdi
+            mov rsi, LENGTH ; copy length of byte array into rsi
+            call _read ; execute system call
+            mov r8, rax ; backup input length
+
+            mov rax, [BUFFER] ; read first value from buffer
+            sub rax, 48 ; convert to number
+            sar rax, 1 ; divide by 2
+            add rax, 48 ; convert to string
+            mov [BUFFER], rax ;write back
+
+            mov rax, r8
+            sub rax, 1
+            ; write to console from buffer
+            mov rdi, BUFFER ; copy pointer to BUFFER into rdi
+            mov rsi, rax ; copy number of bytes to output to rsi
+            call _write ; execute system call
 
 ; exit program with exit code 0
 exit:       mov   rdi, 0                ; first parameter: set exit code
