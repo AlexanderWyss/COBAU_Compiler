1fc96705e24ef090285706f51c650366729ad79d
Alexander Wyss <alexsilvan.wyss@gmail.com>
2023-09-22 14:05:56 +0200

    comments and refactoring

--- src/div2/asm/div2.asm
+++ src/div2/asm/div2.asm
@@ -7,10 +7,10 @@ extern _exit
 
 ; export entry point
 global _start
-    LENGTH EQU 20
+    LENGTH EQU 22
 section .bss ; uninitialized data
     alignb 8 ; align to 8 bytes (for 64-bit machine)
-    BUFFER resb LENGTH ; buffer (64 bytes)
+    BUFFER resb LENGTH ; buffer (22 Byte, max number of chars used to represent 64bit int with - and \n\r)
 
 Section .data
   TEXTINPUT: db 'Please enter an integer value:', 13, 10
@@ -25,102 +25,98 @@ _start:
                                         ; ensure that the stack is aligned for the subsequent
                                         ; function calls. Required for Windows and MacOS.
 readInput:
-            mov rdi, TEXTINPUT
+            mov rdi, TEXTINPUT ; write input request
             mov rsi, TEXTINPUTLEN
             call _write
-            mov rdi, BUFFER ; copy pointer to BUFFER into rdi
-            mov rsi, LENGTH ; copy length of byte array into rsi
-            call _read ; read input, length of input in rax
+            mov rdi, BUFFER ; read input into buffer and actual input length in rax
+            mov rsi, LENGTH
+            call _read
 
-bufferToNumber: ; expects buffer initialized and length in rax
+bufferToNumber: ; expects buffer filled with input, length of input in rax
             mov r9, rax ; mover buffer length to r9
             mov r8, 0 ; init number with zero in r8
-            mov rcx, 0 ; init loop counter to rcx
+            mov rcx, 0 ; init loop index to rcx
 
 bufferToNumberLoop: ; r8=number, rcx=index, r9=buffer_length
             mov rax, BUFFER ; set rax to start of buffer
-            add rax, r9 ; add length to position of buffer
-            sub rax, rcx
-            sub rax, 1
+            add rax, r9 ; add length to position of buffer (start and end of buffer)
+            sub rax, 1 ; subtract 1 from current position (length - 1 -> end of buffer)
+            sub rax, rcx ; subtract current index (from end of buffer minus current index)
             movzx rdx, byte [rax] ; read value of buffer
-            cmp rdx, '-'
+            cmp rdx, '-' ; if value is '-' negate number and goto divide
             je negateNumber
             cmp rdx, 48 ; filter non ascii
-            jl shortenLoop
+            jl ignoreCharacter
             cmp rdx, 57 ; filter non ascii
-            jg shortenLoop
+            jg ignoreCharacter
             sub rdx, 48 ; convert ascii to number
-            cmp rcx, 0 ; if index is 0 10^0
+            cmp rcx, 0 ; if index is 0 do not multiply by 10
             je bufferToNumberOfTenthPowerLoopEnd
             mov rax, 0 ; init index of tenth power loop
 bufferToNumberTenthPowerLoopStart:
             imul rdx, 10 ; multiply number by 10
             add rax, 1 ; add 1 to inner loop counter
-            cmp rax, rcx ; if inner loop lower then outerloop multipl by ten again
+            cmp rax, rcx ; if inner loop lower then outerloop multiply by ten again (eg. 10^1, 10^2, ...)
             jl bufferToNumberTenthPowerLoopStart
 bufferToNumberOfTenthPowerLoopEnd:
-            add rdx, r8 ; add digit to number
-            mov r8, rdx ; write back number
+            add r8, rdx ; add digit to number
             add rcx, 1; increment index
-            cmp rcx, r9
+            cmp rcx, r9 ; if current index is lower then length of buffer, read next digit
             jl bufferToNumberLoop
-            jmp divide
-shortenLoop:
-    sub r9, 1
-    jmp bufferToNumberLoop
+            jmp divide ; jump to divide, ignoreCharacter and negateNumber are helpers
+ignoreCharacter:
+    sub r9, 1 ; subract 1 from max length, combined with index this ignores the current character
+    jmp bufferToNumberLoop ; next character
 negateNumber:
-    sub r9, 1
-    neg r8
+    neg r8 ; negate number
 
-divide:
-            mov rdx, 0
+divide: ; number saved in r8
+            mov rdx, 0 ; idiv works with 128 bit (rdx:rax), we work with 64bit ints, rdx always zero
             mov rax, r8
-            cqo ; sign extend
-            mov r10, 2
-            idiv r10
-            mov r8, rax
+            cqo ; sign extend, when the number is negative the sign bit is rax, idiv works wih rdx:rax, cqo fixes the sign value
+            mov r10, 2; init r10 with 2
+            idiv r10 ; divide by 2
+            mov r8, rax ; write divided number back to r8
 
 numberToBuffer: ; r8=number, rbx=buffer begin address
-            mov rcx, 0 ; buffer length
-            mov rbx, BUFFER
+            mov rcx, 0 ; init loop index
+            mov rbx, BUFFER ; point to end of buffer with rbx
             add rbx, LENGTH
-            mov r12, 0 ; 0 = positive
+            mov r12, 0 ; remember if number is positive or negative: 0 = positive
             cmp r8, 0
             jge numberToBufferLoopStart
             mov r12, 1 ; 1 = negative
 
 numberToBufferLoopStart:
             sub rbx, 1 ; buffer index -1
-            mov rdx, 0
+            mov rdx, 0 ; same as divide
             mov rax, r8
             cqo ; sign extend
             mov r10, 10
             idiv r10
-            cmp r12, 0
+            cmp r12, 0 ; when negative, negate current digit for printing
             je numberToBufferAdd
             neg rdx
 numberToBufferAdd:
-            add rdx, 48
-            mov [rbx], dl
-            mov r8, rax
-            add rcx, 1
-            cmp rax, 0
+            add rdx, 48; convert to ascii
+            mov [rbx], dl ; write remainder to buffer
+            mov r8, rax ; write result to current number
+            add rcx, 1 ; increment index
+            cmp rax, 0 ; if result is zero, end loop
             jne numberToBufferLoopStart
-            cmp r12, 0
+            cmp r12, 0 ; if number is negative append '-'
             je writeBuffer
             sub rbx, 1
             add rcx, 1
             mov [rbx], byte '-'
 
-
-
 writeBuffer:
-            mov rdi, TEXTOUTPUT
+            mov rdi, TEXTOUTPUT ; wirte output text
             mov rsi, TEXTOUTPUTLEN
             call _write
-            mov rdi, rbx ; copy pointer to BUFFER into rdi
-            mov rsi, rcx ; copy number of bytes to output to rsi
-            call _write ; execute system call
+            mov rdi, rbx ; write result
+            mov rsi, rcx
+            call _write
 
 ; exit program with exit code 0
 exit:       mov   rdi, 0                ; first parameter: set exit code
