998d683e6fb19ee4479ff66eba07b130000c3ab4
Alexander Wyss <alexsilvan.wyss@gmail.com>
2023-11-21 20:52:14 +0100

    milestone 3 submission

--- src/main/java/ch/hslu/cobau/minij/symboltable/Entity.java
+++
@@ -1,5 +0,0 @@
-package ch.hslu.cobau.minij.symboltable;
-
-public enum Entity {
-    VARIABLE, RECORD, FUNCTION
-}
--- src/main/java/ch/hslu/cobau/minij/symboltable/Symbol.java
+++
@@ -1,7 +0,0 @@
-package ch.hslu.cobau.minij.symboltable;
-
-
-import ch.hslu.cobau.minij.ast.type.Type;
-
-public record Symbol(String identifier, Entity entity, Type type) {
-}
